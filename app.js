294dcae449772d624b454199fde84c1831682f06
Tang Ery <mrtev008@gmail.com>
2026-09-16 15:55:32 +0700

    demo

---
+++ app.js
@@ -0,0 +1,11 @@
+// const math = require("./math.js");
+
+// console.log(math.add(2, 3));
+// console.log(math.subtract(5, 2));
+// console.log(math.multiply(4, 3));
+// console.log(math.divide(10, 2));
+
+const exercise = require("./exercise.js");
+console.log(exercise.getName());
+console.log(exercise.getAge());
+console.log(exercise.getGrade());
---
+++ exercise.js
@@ -0,0 +1,11 @@
+function getName() {
+    return "Name: Chrea Virin";
+}
+function getAge() {
+    return "Age: " + 25;
+}
+function getGrade() {
+    return "Grade: A";
+}
+
+module.exports = { getName, getAge, getGrade };
